5c2fbee6ce184bbc7b8fb4ea4c803447d871b6b9
Noel Delgado <noeldelgadom@gmail.com>
2018-03-08 15:20:58 -0600

    58. Presenting a Modal

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,6 +1,8 @@
+import { QuotePage } from './../quote/quote';
 import { QuotesService } from './../../service/quotes';
 import { Component } from '@angular/core';
 import { Quote } from '../../data/quote.interface';
+import { ModalController } from 'ionic-angular';
 
 @Component({
   selector: 'page-favorites',
@@ -10,10 +12,16 @@ export class FavoritesPage {
   quotes: Quote[];
 
   constructor(
-    private quotesService: QuotesService
+    private quotesService: QuotesService,
+    private modalController: ModalController
   ) {}
 
   ionViewWillEnter() {
     this.quotes = this.quotesService.getFavoriteQuotes();
+  }
+
+  onViewQuote(quote: Quote) {
+    const modal = this.modalController.create(QuotePage);
+    modal.present();
   }
 }
